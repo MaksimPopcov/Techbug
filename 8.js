eda65d389003326de93914adaad081ab857051c2
Maksim Popcov <maksims.popcovs@gmail.com>
2017-03-07 19:32:11 +0000

    nr8 redone

--- 8.js
+++ 8.js
@@ -1,20 +1,3 @@
-//----------------------------------------8. Even or Odd
-
-function oddEven () {
-	for (var counter = 1; counter < 16; counter++) {
-
-	    if (counter % 2 == 0) {
-	      console.log(counter + " is even");  
-	    } else {
-
-	      console.log(counter + " is odd");  
-	    }
-	}
-}
-oddEven();
-
-
-
 
 function evenOdd () {
 
@@ -28,15 +11,4 @@ function evenOdd () {
 	}
 }
 
-evenOdd();
-
-
-
-function oddOrEven(15){
-  		for (i = 1; i < 15; i++) {
-	        if(i % 2 == 0)
-	            return "even";
-	        return "odd";
-	    }
-    }
-oddOrEven(i);
+evenOdd(i);
